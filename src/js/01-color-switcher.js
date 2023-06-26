59aa44e82f9f28f6cb03683807cb4e5fab2c9998
Анна Котляр <kotlyaranya1771@gmail.com>
2023-06-26 19:43:37 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -23,6 +23,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
 
+// 2 variant
 // const startBtn = document.querySelector('[data-start]');
 // const stopBtn = document.querySelector('[data-stop]');
 // const buttons = document.querySelectorAll('[data-start], [data-stop]');
